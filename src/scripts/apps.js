74f647dc36a7e386836ebb82d79aa03914d960a7
Will Cook <wellsee83@gmail.com>
2016-08-29 16:09:51 -0500

    Header and Nav are styled, Scroll to hide nav function works

---
+++ src/scripts/apps.js
@@ -0,0 +1,32 @@
+var didScroll;
+var lastScrollTop = 0;
+var delta = 10;
+var navbarHeight = $('nav').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('nav').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('nav').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+}
